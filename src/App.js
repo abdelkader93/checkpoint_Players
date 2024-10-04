304db864d29053c872c563d876711e77d1780c2e
Abdelkader.ferchichi <abdelkader.ferchichi@esprit.tn>
2024-10-04 11:17:32 +0100

    checkpoint players

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// App.js
+import React from 'react';
+import './App.css';
+import PlayersList from './components/PlayersList';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FIFA Player Cards</h1>
+      <PlayersList />    </div>
+  );
+}
+
+export default App;
---
+++ src/players.js
@@ -0,0 +1,37 @@
+// players.js
+const players = [
+    {
+        name: "Lionel Messi",
+        team: "Inter Miami",
+        nationality: "Argentina",
+        jerseyNumber: 10,
+        age: 36,
+        imageUrl: "https://cdn.britannica.com/35/238335-050-2CB2EB8A/Lionel-Messi-Argentina-Netherlands-World-Cup-Qatar-2022.jpg"
+    },
+    {
+        name: "Cristiano Ronaldo",
+        team: "Al-Nassr",
+        nationality: "Portugal",
+        jerseyNumber: 7,
+        age: 39,
+        imageUrl: "https://hips.hearstapps.com/hmg-prod/images/cristiano-ronaldo-of-portugal-reacts-as-he-looks-on-during-news-photo-1725633476.jpg?crop=0.666xw:1.00xh;0.180xw,0&resize=640:*"
+    },
+    {
+        name: "Kylian Mbappe",
+        team: "Paris Saint-Germain",
+        nationality: "France",
+        jerseyNumber: 7,
+        age: 25,
+        imageUrl: "https://i.ytimg.com/vi/cgGZK8sMcV8/maxresdefault.jpg"
+    },
+    {
+        name: "Kevin De Bruyne",
+        team: "Manchester City",
+        nationality: "Belgium",
+        jerseyNumber: 17,
+        age: 32,
+        imageUrl: "https://lh3.googleusercontent.com/31JXx8yX9o4z4fpK71MSoDbt4xFJlBy9nJM_OHjidhmpCKCtvEB8fB38IohFESD_Sy1xdikY0iBJqJ8siuVf6lngNDO5syYpqOEC=w300-h250-p-e365"
+    }
+];
+
+export default players;
